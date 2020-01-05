c9e3444944195592d6e0cca9c5536c733137d83e
Kotes <51745529+KotesUA@users.noreply.github.com>
2020-01-05 23:02:29 +0200

    Добавьте файлы проекта.

---
+++ MP3Helper/Form1.Designer.cs
@@ -0,0 +1,371 @@
+﻿namespace MP3Helper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.performersRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.albumTextBox = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.yearTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.filenameTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.genresRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.bpmTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.commentRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.saveChangesButton = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.selectFolderButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel8.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(407, 368);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Performers:";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.performersRichTextBox);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(426, 42);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(362, 65);
+            this.panel1.TabIndex = 3;
+            // 
+            // performersRichTextBox
+            // 
+            this.performersRichTextBox.Location = new System.Drawing.Point(62, 3);
+            this.performersRichTextBox.Name = "performersRichTextBox";
+            this.performersRichTextBox.Size = new System.Drawing.Size(297, 59);
+            this.performersRichTextBox.TabIndex = 3;
+            this.performersRichTextBox.Text = "";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.albumTextBox);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(621, 113);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(167, 27);
+            this.panel2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Album:";
+            // 
+            // albumTextBox
+            // 
+            this.albumTextBox.Location = new System.Drawing.Point(61, 3);
+            this.albumTextBox.Name = "albumTextBox";
+            this.albumTextBox.Size = new System.Drawing.Size(102, 20);
+            this.albumTextBox.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.yearTextBox);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Location = new System.Drawing.Point(622, 217);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(166, 27);
+            this.panel3.TabIndex = 5;
+            // 
+            // yearTextBox
+            // 
+            this.yearTextBox.Location = new System.Drawing.Point(61, 3);
+            this.yearTextBox.Name = "yearTextBox";
+            this.yearTextBox.Size = new System.Drawing.Size(102, 20);
+            this.yearTextBox.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 6);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Year:";
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.filenameTextBox);
+            this.panel4.Controls.Add(this.label4);
+            this.panel4.Location = new System.Drawing.Point(426, 12);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(362, 27);
+            this.panel4.TabIndex = 6;
+            // 
+            // filenameTextBox
+            // 
+            this.filenameTextBox.Location = new System.Drawing.Point(62, 3);
+            this.filenameTextBox.Name = "filenameTextBox";
+            this.filenameTextBox.Size = new System.Drawing.Size(297, 20);
+            this.filenameTextBox.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 6);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Filename:";
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.genresRichTextBox);
+            this.panel6.Controls.Add(this.label6);
+            this.panel6.Location = new System.Drawing.Point(426, 146);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(362, 65);
+            this.panel6.TabIndex = 4;
+            // 
+            // genresRichTextBox
+            // 
+            this.genresRichTextBox.Location = new System.Drawing.Point(62, 3);
+            this.genresRichTextBox.Name = "genresRichTextBox";
+            this.genresRichTextBox.Size = new System.Drawing.Size(297, 59);
+            this.genresRichTextBox.TabIndex = 3;
+            this.genresRichTextBox.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 6);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Genres:";
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.bpmTextBox);
+            this.panel5.Controls.Add(this.label5);
+            this.panel5.Location = new System.Drawing.Point(426, 217);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(167, 27);
+            this.panel5.TabIndex = 5;
+            // 
+            // bpmTextBox
+            // 
+            this.bpmTextBox.Location = new System.Drawing.Point(61, 3);
+            this.bpmTextBox.Name = "bpmTextBox";
+            this.bpmTextBox.Size = new System.Drawing.Size(102, 20);
+            this.bpmTextBox.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(4, 6);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "BPM:";
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.titleTextBox);
+            this.panel7.Controls.Add(this.label7);
+            this.panel7.Location = new System.Drawing.Point(426, 113);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(167, 27);
+            this.panel7.TabIndex = 6;
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.Location = new System.Drawing.Point(61, 3);
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(102, 20);
+            this.titleTextBox.TabIndex = 1;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(4, 6);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(30, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Title:";
+            // 
+            // panel8
+            // 
+            this.panel8.Controls.Add(this.commentRichTextBox);
+            this.panel8.Controls.Add(this.label8);
+            this.panel8.Location = new System.Drawing.Point(426, 250);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(362, 65);
+            this.panel8.TabIndex = 5;
+            // 
+            // commentRichTextBox
+            // 
+            this.commentRichTextBox.Location = new System.Drawing.Point(62, 3);
+            this.commentRichTextBox.Name = "commentRichTextBox";
+            this.commentRichTextBox.Size = new System.Drawing.Size(297, 59);
+            this.commentRichTextBox.TabIndex = 3;
+            this.commentRichTextBox.Text = "";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(3, 6);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(54, 13);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Comment:";
+            // 
+            // saveChangesButton
+            // 
+            this.saveChangesButton.Location = new System.Drawing.Point(426, 322);
+            this.saveChangesButton.Name = "saveChangesButton";
+            this.saveChangesButton.Size = new System.Drawing.Size(362, 111);
+            this.saveChangesButton.TabIndex = 7;
+            this.saveChangesButton.Text = "Save changes";
+            this.saveChangesButton.UseVisualStyleBackColor = true;
+            this.saveChangesButton.Click += new System.EventHandler(this.saveChangesButton_Click);
+            // 
+            // selectFolderButton
+            // 
+            this.selectFolderButton.Location = new System.Drawing.Point(13, 388);
+            this.selectFolderButton.Name = "selectFolderButton";
+            this.selectFolderButton.Size = new System.Drawing.Size(407, 45);
+            this.selectFolderButton.TabIndex = 8;
+            this.selectFolderButton.Text = "Select folder";
+            this.selectFolderButton.UseVisualStyleBackColor = true;
+            this.selectFolderButton.Click += new System.EventHandler(this.selectFolderButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.selectFolderButton);
+            this.Controls.Add(this.saveChangesButton);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
+            this.panel8.ResumeLayout(false);
+            this.panel8.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox performersRichTextBox;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox albumTextBox;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox yearTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TextBox filenameTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.RichTextBox genresRichTextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.TextBox bpmTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.TextBox titleTextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.RichTextBox commentRichTextBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button saveChangesButton;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button selectFolderButton;
+    }
+}
+
---
+++ MP3Helper/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using TagLib;
+using System.IO;
+using System.Diagnostics;
+
+namespace MP3Helper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            directories = new List<string>();
+            ChooseDirectory();
+        }
+
+        private static List<string> directories;
+
+        private void OpenMP3(string path)
+        {
+            var audioFile = TagLib.File.Create(path);
+            performersRichTextBox.Text = audioFile.Tag.JoinedPerformers;
+            albumTextBox.Text = audioFile.Tag.Album;
+            yearTextBox.Text = audioFile.Tag.Year.ToString();
+            genresRichTextBox.Text = audioFile.Tag.JoinedGenres;
+            bpmTextBox.Text = audioFile.Tag.BeatsPerMinute.ToString();
+            titleTextBox.Text = audioFile.Tag.Title;
+            commentRichTextBox.Text = audioFile.Tag.Comment;
+            filenameTextBox.Text = Path.GetFileName(path);
+        }
+
+        private void WriteToMP3(string path)
+        {
+            var audioFile = TagLib.File.Create(path);
+            audioFile.Tag.Performers = performersRichTextBox.Text.Split(';');
+            audioFile.Tag.Album = albumTextBox.Text;
+            audioFile.Tag.Year = Convert.ToUInt32(yearTextBox.Text);
+            audioFile.Tag.Genres = genresRichTextBox.Text.Split(';');
+            audioFile.Tag.BeatsPerMinute = Convert.ToUInt32(bpmTextBox.Text);
+            audioFile.Tag.Title = titleTextBox.Text;
+            audioFile.Tag.Comment = commentRichTextBox.Text;
+
+            System.IO.File.Move(path, filenameTextBox.Text);
+        }
+
+        private void ChooseDirectory()
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                directories = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.mp3*", SearchOption.AllDirectories).ToList();
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            OpenMP3(directories[listBox1.SelectedIndex]);
+        }
+
+        private void saveChangesButton_Click(object sender, EventArgs e)
+        {
+            WriteToMP3(directories[listBox1.SelectedIndex]);
+            MessageBox.Show("Changes saved!");
+
+            directories = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.mp3*", SearchOption.AllDirectories).ToList();
+            listBox1.DataSource = null;
+            listBox1.DataSource = directories;
+        }
+
+        private void selectFolderButton_Click(object sender, EventArgs e)
+        {
+            ChooseDirectory();
+            listBox1.DataSource = directories;
+        }
+    }
+}
